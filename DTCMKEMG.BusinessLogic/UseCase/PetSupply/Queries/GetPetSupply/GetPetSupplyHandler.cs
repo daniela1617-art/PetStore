4d5ea64f2ad9d23b0712935f15e41bc31ffb3bf4
Kevin Ernesto Meza Garcia <erasmomeza11@gmail.com>
2026-05-13 00:18:03 -0600

    feat-Kevin Ernesto Meza Garcia

---
+++ DTCMKEMG.BusinessLogic/UseCase/PetSupply/Queries/GetPetSupply/GetPetSupplyHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTCMKEMG.BusinessLogic.UseCase.PetSupply.Queries.GetPetSupply
+{
+    internal class GetPetSupplyHandler
+    {
+    }
+}
